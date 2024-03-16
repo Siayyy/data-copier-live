05b85dee0f262adb38fb18a3d0d3c235c9bad1b2
Siayyy <131888565+Siayyy@users.noreply.github.com>
2024-03-16 19:59:15 +0000

    added logic to read table_list that need to be loaded

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import sys
-
 from config import DB_DETAILS
-
+from util import get_tables
 
 def main():
-    env = sys.argv[1]
-    db_details = DB_DETAILS[env]
-    print(db_details)
-
-
-    if __name__ == '__main__':
-        main()
+    try:
+        env = sys.argv[1]
+    except IndexError:
+        print("No environment provided. Defaulting to 'dev'.")
+        env = 'dev'  # 使用默认值
+    db = DB_DETAILS[env]
+    tables = get_tables('table_list.txt')
+    # for idx, table in tables.iterrows():
+    #only output table name
+    for table in tables['table_name']:
+        print(table)
+if __name__ == '__main__':
+    main()
